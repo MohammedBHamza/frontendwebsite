991feda46ed1da66ac3ae981950e55d251264161
MohammedBHamza <58260725+MohammedBHamza@users.noreply.github.com>
2019-12-03 22:33:05 +0100

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
+
+/* eslint-env es6 */
+/* eslint-disable no-console */
+
